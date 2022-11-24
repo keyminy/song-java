1969033197b0887b7ec8cf0da0bbe594dee2b108
keyminy <imanaifs@gmail.com>
2022-11-24 19:18:34 +0900

    common exception

--- src/main/java/kr/co/songjava/mvc/controller/BoardController.java
+++ src/main/java/kr/co/songjava/mvc/controller/BoardController.java
@@ -3,6 +3,7 @@ package kr.co.songjava.mvc.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import kr.co.songjava.configuration.exception.BaseException;
+import kr.co.songjava.configuration.http.BaseResponse;
+import kr.co.songjava.configuration.http.BaseResponseCode;
 import kr.co.songjava.mvc.domain.Board;
 import kr.co.songjava.mvc.service.BoardService;
 
@@ -27,16 +31,21 @@ public class BoardController {
 	
 	@GetMapping
 	@ApiOperation(value="목록 조회",notes = "게시물 목록 정보를 조회할 수 있습니다.")
-	public List<Board> getList(){
-		return boardService.getList();
+	public BaseResponse<List<Board>> getList(){
+		return new BaseResponse<>(boardService.getList());
 	};
 	@GetMapping("/{boardSeq}")
 	@ApiOperation(value="상세 조회",notes = "게시물 번호에 해당하는 상세 정보를 조회할 수 있습니다.")
 	@ApiImplicitParams({
 		@ApiImplicitParam(name = "boardSeq",value="게시물 번호",example="1")
 	})
-	public Board get(@PathVariable int boardSeq) {
-		return boardService.get(boardSeq);
+	public BaseResponse<Board> get(@PathVariable int boardSeq) {
+		Board board = boardService.get(boardSeq);
+		//null처리
+		if(board == null) {
+			throw new BaseException(BaseResponseCode.DATA_IS_NULL,new String[] {"게시물"});
+		}
+		return new BaseResponse<>(boardService.get(boardSeq));
 	};//단건 조회
 	
 	/**
@@ -50,9 +59,19 @@ public class BoardController {
 		@ApiImplicitParam(name = "title",value="제목",example="spring"),
 		@ApiImplicitParam(name = "contents",value="내용",example="spring 강좌"),
 	})
-	public int save(Board parameter) {
+	public BaseResponse<Integer> save(Board parameter) {
+		//null처리 하기
+		//제목 필수 체크
+		if(StringUtils.isEmpty(parameter.getTitle())) {
+			throw new BaseException(BaseResponseCode.VALIDATE_REQUIRED,new String[] {"title" , "제목"});
+		}
+		//내용 필수 체크
+		if(StringUtils.isEmpty(parameter.getContents())) {
+			throw new BaseException(BaseResponseCode.VALIDATE_REQUIRED,new String[] {"contents" , "내용"});
+		}
+		
 		boardService.save(parameter);
-		return parameter.getBoardSeq();
+		return new BaseResponse<>(parameter.getBoardSeq());
 	};
 
 	
@@ -65,13 +84,13 @@ public class BoardController {
 	@ApiImplicitParams({
 		@ApiImplicitParam(name = "boardSeq",value="게시물 번호",example="1"),
 	})
-	public boolean delete(@PathVariable int boardSeq) {
+	public BaseResponse<Boolean> delete(@PathVariable int boardSeq) {
 		Board board = boardService.get(boardSeq);
 		if(board == null) {
-			return false;
+			return new BaseResponse<Boolean>(false);
 		}
 		boardService.delete(boardSeq);
-		return true;
+		return new BaseResponse<Boolean>(true);
 	};
 	
 }
